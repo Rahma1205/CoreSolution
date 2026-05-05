aa62e8dff7f64d7680aaf938fd38e64fe31c422c
Rahma Osama <rahmaosama724@gmail.com>
2026-05-06 01:27:51 +0300

    update project

--- src/email.js
+++ src/email.js
@@ -2,16 +2,19 @@ import emailjs from "@emailjs/browser";
 
 export default function sendEmail(values) {
   return emailjs.send(
-    process.env.REACT_APP_SERVICE_NAME,
-    process.env.REACT_APP_TEMPLATE_ID,
+    // process.env.REACT_APP_SERVICE_NAME ,
+    "service_ho0ede4",
+    // process.env.REACT_APP_TEMPLATE_ID,
+    "template_uwzfdi8",
     {
-      to_email: process.env.REACT_APP_EMAIL_USER,
+      to_email:"morsy.ahmedd2@gmail.com",
       first_name: values.firstName,
       last_name: values.lastName,
       company: values.company,
       industry: values.industry,
       message: values.message,
     },
-    process.env.REACT_APP_PUBLIC_KEY
+    // process.env.REACT_APP_PUBLIC_KEY
+    "bzCT_YcEsPABijuvh"
   );
 }
